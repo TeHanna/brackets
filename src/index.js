870dba915de4c6f07aca09440ee188aff26d98a6
TeHanna <68251610+TeHanna@users.noreply.github.com>
2021-08-31 02:51:42 +0300

    feat: 'change index.js'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const leftBrackets = ['(', '[', '|'];
+  const pairBrackets = {
+    [')']: '(',
+    [']']: '[',
+    ['|']: '|',
+
+  };
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let curSymbol = str[i];
+    if (leftBrackets.includes(curSymbol)) {
+      stack.push(curSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let lastElement = stack[stack.length - 1];
+      if (pairBrackets[curSymbol] === lastElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+    return stack.length === 0;
+  }
 }
+
